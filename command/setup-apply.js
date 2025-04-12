a1c387efee0c880785df4592f635947ce6ef4cd8
Wick Studio <altybyk748@gmail.com>
2025-04-12 10:35:39 +0300

    Update setup-apply.js

--- command/setup-apply.js
+++ command/setup-apply.js
@@ -1,11 +1,21 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require("discord.js");
 const config = require("../config.js");
+const fs = require("fs");
+const path = require("path");
+const statusPath = path.join(__dirname, "../applyStatus.json");
+
+function readApplyStatus() {
+    const data = fs.readFileSync(statusPath, "utf8");
+    return JSON.parse(data).open;
+}
 
 module.exports = {
     name: "setup-apply",
     async execute(message, args) {
         if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) return;
 
+        const isApplyOpen = readApplyStatus();
+        
         const embed = new EmbedBuilder()
             .setAuthor({
                 name: message.guild.name,
@@ -19,7 +29,11 @@ module.exports = {
             new ButtonBuilder()
                 .setCustomId("apply")
                 .setLabel(config.apply.name)
-                .setStyle(ButtonStyle[config.apply.colorButton])
+                .setStyle(ButtonStyle[config.apply.colorButton]),
+            new ButtonBuilder()
+                .setCustomId("status")
+                .setEmoji(isApplyOpen ? config.apply.emojiOpen : config.apply.emojiClosed)
+                .setStyle(ButtonStyle.Secondary)
         );
 
         await message.channel.send({ embeds: [embed], components: [row] });
